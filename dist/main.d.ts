2c2fa48e686dd394bfc5abe7ceaca597de97c778
yaz <AAhmed@eimagine.com>
2025-05-10 00:01:29 -0400

    Version update

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,22 +1,23 @@
 import * as form_runner from 'form-runner';
 import { IFormValidator, IValidationMessage, FormStateConfig, FormFieldState } from 'form-runner';
+import * as mutation_tracker from 'mutation-tracker';
 
 interface KeyValuePair {
     [field: string]: any;
 }
 declare function useFormRunner<T extends KeyValuePair>(validator: IFormValidator<IValidationMessage>, dataObject: T, config?: FormStateConfig): {
     errorFlatList: IValidationMessage[];
-    errors: MutatedAttribute<T_1, string[]>;
-    touched: MutatedAttribute<T_1, boolean>;
-    dirty: MutatedAttribute<T_1, boolean>;
+    errors: mutation_tracker.MutatedAttribute<T, string[]>;
+    touched: mutation_tracker.MutatedAttribute<T, boolean>;
+    dirty: mutation_tracker.MutatedAttribute<T, boolean>;
     isSubmitting: boolean;
     isFormDirty: () => boolean;
     isFormTouched: () => boolean;
     isFormValid: () => boolean;
     validateAsync: (model: T) => Promise<boolean>;
-    validate: () => boolean;
+    validate: (model: T) => boolean;
     setIsSubmitting: (isSubmitting: boolean) => void;
-    getFieldState: <T_2>(name: string, currentValue: T_2, previousValue: T_2) => form_runner.FormFieldState<T_2>;
+    getFieldState: <T_1>(name: string, currentValue: T_1, previousValue: T_1) => form_runner.FormFieldState<T_1>;
     getFieldTouched: (fieldName: string) => boolean;
     setFieldTouched: (value: boolean, fieldName: string) => void;
     setFieldsTouched: (value: boolean, fieldNames: string[]) => void;
@@ -33,10 +34,10 @@ declare function useFormFieldState<T extends {
     [field: string]: any;
 }>(validator: IFormValidator<IValidationMessage>, dataObject: T, config?: FormStateConfig): {
     errorFlatList: IValidationMessage[];
-    errors: MutatedAttribute<T_1, string[]>;
-    touched: MutatedAttribute<T_1, boolean>;
-    dirty: MutatedAttribute<T_1, boolean>;
-    getFieldState: <T_2>(name: string, currentValue: T_2, previousValue: T_2) => FormFieldState<T_2>;
+    errors: mutation_tracker.MutatedAttribute<T, string[]>;
+    touched: mutation_tracker.MutatedAttribute<T, boolean>;
+    dirty: mutation_tracker.MutatedAttribute<T, boolean>;
+    getFieldState: <T_1>(name: string, currentValue: T_1, previousValue: T_1) => FormFieldState<T_1>;
     getFieldTouched: (fieldName: string) => boolean;
     setFieldTouched: (value: boolean, fieldName: string) => void;
     setFieldsTouched: (value: boolean, fieldNames: string[]) => void;
